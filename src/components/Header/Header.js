887cdd7e7c5004897ba41c52206b1699e255c06d
Abid-Hasan <ahsnigdho@gmail.com>
2021-10-20 18:46:41 +0600

    header logo imported

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../../images/logo.png'
 
 const Header = () => {
     return (
         <div>
             <h2>Header component</h2>
+            <img src={logo} alt="" />
         </div>
     );
 };
