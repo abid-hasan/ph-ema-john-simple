3ae0367ebd368f45d6272c7de4c69b5d421b2e2a
Abid-Hasan <ahsnigdho@gmail.com>
2021-10-27 07:43:10 +0600

    Search and dynamic rating done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
+import { faCartPlus, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons';
 import './Product.css';
+import Rating from 'react-rating';
 
 const Product = (props) => {
-    const { name, img, seller, price, stock } = props.product;
+    const { name, img, seller, price, stock, star } = props.product;
     return (
         <div className="product">
             <div className="product-image">
@@ -14,13 +16,18 @@ const Product = (props) => {
                 <h4 className="product-name">{name}</h4>
                 <p><small>by: {seller}</small></p>
                 <p>Price: {price}</p>
+                <Rating
+                    readonly
+                    initialRating={star}
+                    emptySymbol={<FontAwesomeIcon icon={faStarRegular} />}
+                    fullSymbol={<FontAwesomeIcon className="full-star" icon={faStar} />}
+                />
                 <p><small>Only {stock} left in stock. Order soon.</small></p>
                 <button
                     className="btn-regular"
                     onClick={() => props.handleAddToCart(props.product)}
                 >
-                    <FontAwesomeIcon className="fa-icon" icon={faCartPlus} />
-                    Add to cart
+                    <FontAwesomeIcon icon={faCartPlus} /> Add to cart
                 </button>
             </div>
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,11 +7,15 @@ import './Shop.css';
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
+    const [displayProducts, setDisplayProducts] = useState([]);
 
     useEffect(() => {
         fetch('./products.JSON')
             .then(res => res.json())
-            .then(data => setProducts(data));
+            .then(data => {
+                setProducts(data);
+                setDisplayProducts(data);
+            });
     }, []);
 
     useEffect(() => {
@@ -34,19 +38,33 @@ const Shop = () => {
         addToDb(product.key);
     };
 
+    const handleSearch = (event) => {
+        const searchText = event.target.value;
+        const matchedProducts = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()));
+        setDisplayProducts(matchedProducts);
+    };
+
     return (
-        <div className="shop-container">
-            <div className="product-container">
-                {
-                    products.map(product => <Product
-                        key={product.key}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>)
-                }
+        <div>
+            <div className="search-container">
+                <input
+                    onChange={handleSearch}
+                    type="text"
+                    placeholder="Search Product" />
             </div>
-            <div className="cart-container">
-                <Cart cart={cart}></Cart>
+            <div className="shop-container">
+                <div className="product-container">
+                    {
+                        displayProducts.map(product => <Product
+                            key={product.key}
+                            product={product}
+                            handleAddToCart={handleAddToCart}
+                        ></Product>)
+                    }
+                </div>
+                <div className="cart-container">
+                    <Cart cart={cart}></Cart>
+                </div>
             </div>
         </div>
     );
